99b8c7ae7e89543d52434cabfba669f009d335fd
hugobarzano <hugobarzano@gmail.com>
2015-10-22 20:26:55 +0200

    Actualizando documentacion y setup

--- mysite/empresas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from empresas.models import Empresa
-admin.site.register(Empresa)
-
-from empresas.models import Valoracion
-admin.site.register(Valoracion)
